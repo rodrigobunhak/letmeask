cbc44b58409e01825c2dbb6f99e643746769248c
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2021-06-22 17:53:57 -0300

    refactoring context auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
 import { useHistory } from 'react-router-dom';
-import { useContext } from 'react';
-import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -9,10 +7,11 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuths';
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext)
+  const { user, signInWithGoogle } = useAuth()
 
   async function handleCreateRoom() {
 
